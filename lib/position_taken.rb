eadbe0778301733260554b070da785e8ed5c9f20
wesley perez <wesley.m.perez@gmail.com>
2019-11-25 21:45:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,12 @@
 # code your #position_taken? method here!
 
-board = [" "]
-def posistion_taken?(board,positon)
-  if board[position] == " "
-    taken = false
-  else
-    taken = true
-end
+
+def posistion_taken?(board,index)
+taken = nil
+if (board[index] == " " || board[index] == nil)
+  taken = false
+else 
+  taken = true
+ end
 taken
-end
+
