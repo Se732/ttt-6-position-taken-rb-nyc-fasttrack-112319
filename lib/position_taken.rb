d66e258143eb79e00cc6042a58c3526b5a3bf104
wesley perez <wesley.m.perez@gmail.com>
2019-11-25 21:15:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
 # code your #position_taken? method here!
-def posistion_taken?(board,index)
 
+board = [" "]
+def posistion_taken?(board,index)
+  if array[index_number] == board 
+    puts false
+end
 end
